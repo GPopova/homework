16c2fda07614504662449ba5dd0632e9606f268c
Galyna Popova <galka_po@mail.ru>
2017-06-14 15:56:57 +0300

    Lab4-Ok, Improved version

---
+++ jsLab4OK/labFitness/js/script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+//Task1
+
+var elemLogoImg = document.querySelector('img');
+console.log(elemLogoImg);
+
+//or
+
+var elemLogoImg = document.querySelector('div [class = "logo"] img');
+console.log(elemLogoImg);
+
+//Task2
+
+var textLeftMenu = document.getElementById('list').textContent;
+console.log(textLeftMenu);
+
+// Task3
+
+var elemImg = document.querySelector('div [class ="image"] img');
+console.log(elemImg);
+
+//Task4
+
+var textCopyright = document.getElementById('copy').textContent;
+console.log(textCopyright);
